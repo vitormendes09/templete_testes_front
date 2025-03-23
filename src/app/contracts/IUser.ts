b20dd2a5df297f800411c950f3afe0aab6a755c5
Vitor Mendes <mendes.v@gsuite.iff.edu.br>
2025-03-23 11:07:24 -0300

    criated IUser

---
+++ src/app/contracts/IUser.ts
@@ -0,0 +1,9 @@
+export interface IUser {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    birthDay: string;
+    birthMonth: string;
+    birthYear: string;
+  }
